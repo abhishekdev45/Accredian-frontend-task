e896388a0f42d3b399bdf2db424721f7ee3bc049
abhishekdev45 <156638090+AbhishekWebdesys@users.noreply.github.com>
2024-07-15 11:09:16 +0530

    ss

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 // src/axiosConfig.js
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:4000/api';
+const baseURL = process.env.REACT_APP_API_BASE_URL;
 
 const axiosInstance = axios.create({
   baseURL,
